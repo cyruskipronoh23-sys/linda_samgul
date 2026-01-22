a617e270bfadb2337636361c6107a92949cd9096
cyruskipronoh23-sys <cyrus.kipronoh23@s.karu.ac.ke>
2026-01-22 14:11:50 +0300

    Create BackgroundAnimation component for animated background circles

---
+++ src/components/BackgroundAnimation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BackgroundAnimation.css';
+
+const BackgroundAnimation = () => {
+  return (
+    <div className="background-animation">
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
+      <div className="circle circle3"></div>
+      <div className="circle circle4"></div>
+    </div>
+  );
+};
+
+export default BackgroundAnimation;
